02584f1ef2592ebec3f9e04744a2ecb35fcb74d6
Maryam6713 <maryammalik6713@gmail.com>
2024-05-09 15:55:50 +0500

    initial commit

---
+++ index.js
@@ -0,0 +1,82 @@
+//--------------chapter Alert--------------//
+
+var error = "Error! Please enter a valid password.";
+alert(error)
+
+var welcome = "Welcome to JS Land... \n Happy Coding!";
+alert(welcome)
+
+var welcome = "Hello... I can runJS through my web browser's console!";
+alert(welcome)
+
+
+// ------------veriable for string---------//
+var username = "Maryam Malik";
+alert(username);
+
+var age = "15 years old";
+alert(age);
+
+var application = "Certified Mobile Application Development";
+alert(application);
+
+var pizza = "PIZZA \n PIZZ \n PIZ \n PI \n P";
+alert(pizza);
+
+var email = "My email address is maryammalik6713@gmail.com";
+alert(email);
+
+var book = "I am trying to learn from the Book A Smarter way to learn JavaScript."
+alert(book)
+
+var browser = "---------**----------"
+alert(browser)
+document.write("-------------------**----------------------")
+
+//-------------veriable for number-------------//
+var age = 20
+alert("I am " + age + "" + "year old.")
+//-------------------------------//
+
+var visitor = 0;
+visitor++;
+document.write("You have visited this site" + visitor + "times.")
+
+//--------------------------------//
+
+var visitorName = "John Doe";
+var productTitle = "T-Shirt";
+var quantity = 5;
+
+document.write(visitorName + "Ordered" + quantity + "" + productTitle + "XYZ Clothing Store.")
+
+///----------------------------------------------------//
+
+var variable1 = 10 , veriable2 = "Hellow" , veriable3 = true ;
+document.write(variable1 , veriable2 , veriable3);
+
+//-------------legal variable name------------------------//
+
+var firstName = "Maryam";
+var my_age = 20;
+var $price = 200 ;
+var isStudent = "my student";
+var totalPrize = "Rs 1000";
+   
+//------------------illegal veriable name------------//
+
+var 123Abc ;
+var my-veriable ;
+var @username ;
+var true ;
+var var ;
+
+//--------------------------------//
+
+//------------------------------------//
+document.write("<h1>Rules for naming JS variables</h1>");
+document.write("Variable names can only contain letters, numbers, underscores, and dollar signs. For example, $my_1stVariable<br>");
+document.write("Variables must begin with a letter, underscore, or dollar sign. For example, $name, _name, or name<br>");
+document.write("Variable names are case-sensitive<br>");
+document.write("Variable names should not be JavaScript keywords<br>");
+
